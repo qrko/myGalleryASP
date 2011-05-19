7300d0e90f518372d7a43ad7b2a0b09c5410fa64
Andrzej Stolarczyk <andrewspec@live.com>
2011-05-19 15:29:35 +0200

    Dodano skrypty do stworzenia bazy danych

---
+++ HostingGrafikiMVC/Baza/TabelaUsuwanie.sql
@@ -0,0 +1,35 @@
+USE [HostingGrafiki]
+GO
+
+/****** Object:  Table [dbo].[Usuwanie]    Script Date: 05/19/2011 15:28:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Usuwanie](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDPliku] [int] NOT NULL,
+	[GUID] [varchar](30) NOT NULL,
+ CONSTRAINT [PK_Usuwanie] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Usuwanie]  WITH CHECK ADD  CONSTRAINT [FK_Usuwanie_Pliki] FOREIGN KEY([IDPliku])
+REFERENCES [dbo].[Pliki] ([ID])
+GO
+
+ALTER TABLE [dbo].[Usuwanie] CHECK CONSTRAINT [FK_Usuwanie_Pliki]
+GO
+
